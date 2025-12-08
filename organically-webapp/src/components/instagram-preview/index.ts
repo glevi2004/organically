81e9c6cc1758c59d1b55afa126564b2bfbfebe16
Levi Ramos <leviramosgabriel2021@gmail.com>
2025-12-08 02:41:20 -0500

    Export IPhoneFrame and InstagramPhonePreview components

--- organically-webapp/src/components/instagram-preview/index.ts
+++ organically-webapp/src/components/instagram-preview/index.ts
@@ -1,5 +1,6 @@
 export { InstagramPreview } from "./InstagramPreview";
 export { InstagramEmbedPreview } from "./InstagramEmbedPreview";
+export { InstagramPhonePreview } from "./InstagramPhonePreview";
 export { MediaUpload } from "./MediaUpload";
 export { IPhoneFrame } from "./IPhoneFrame";
 
